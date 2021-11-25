7af4402e125a3c2e20467fd9c61a2a728831b6c5
reenatt-dotcom <64491890+reenatt-dotcom@users.noreply.github.com>
2021-11-25 00:47:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,64 @@
 /*----- constants -----*/
 const words = ["python", "javascript", "mongodb", "json", "java", "html", "css", "c", "csharp", "golang", "kotlin", "php", "sql", "ruby"]
 const maxWrong = 6
+const letters = "abcdefghijklmnopqrstuvwxyz"
 
 /*----- app's state (variables) -----*/
-let answer = ''
-randomWord()
-let mistakes = 0
-let guessed = []
-let blank = ""
-for (let i = 0; i < answer.length; i++) {
-  blank += "_ "
-}
-render(document.getElementById("blank"), blank)
-let answerwithspaces = ""
-for (let i = 0; i < answer.length; i++) {
-  answerwithspaces += answer[i]
-  answerwithspaces += " "
-}
+let answer
+let mistakes
+let guessed
+let blank
+let answerwithspaces
 let userRating
-let letters = "abcdefghijklmnopqrstuvwxyz"
 
 /*----- cached element references -----*/
 const sixteen = document.getElementById("hangman16")
 
 /*----- event listeners -----*/
+document.getElementById("buttoncontainer").addEventListener("click", handleGuess)
+document.getElementById("reset").addEventListener("click", init)
 
 /*----- functions -----*/
 function randomWord() {
   answer = words[Math.floor(Math.random() * words.length)]
 }
 
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
 // render
 function render(x, y) {
   x.innerHTML = y
-
 }
 
-const fun = function (e) {
+// handleGuess
+function handleGuess(e) {
+
   let number = e.target.id.replace("post-", "")
-
   document.getElementById(number).disabled = "true"
+  guessed.push(letters[number - 1])
   if (e.target && e.target.nodeName == "BUTTON" && e.target.id !== "reset") {
-
     let letter = letters[number - 1]
-    console.log(letter)
     userRating = 1;
-
     if (answer.includes(letter)) {
       document.getElementById(number).className = 'selected';
       render(document.getElementById("blank"), blank)
-
       if (answerwithspaces[0] === letter) {
         blank = blank.replaceAt(0, letter)
       }
       for (let i = 1; i < answerwithspaces.length; i++) {
         if (answerwithspaces[i + 1] === letter) {
-          console.log(i)
           blank = blank.replaceAt(i + 1, letter)
-          // console.log(blank)
         }
-
       }
-
-
       render(document.getElementById("blank"), blank)
       if (!blank.includes("_")) {
-        // console.log(blank)
-
         render(sixteen, "you win")
         for (let i = 1; i < 27; i++) {
           document.getElementById(i).disabled = "true"
         }
       }
-
     } else {
       mistakes += 1
       document.getElementById(number).className = 'notselected';
@@ -103,30 +89,66 @@ const fun = function (e) {
           document.getElementById(i).disabled = "true"
         }
       }
-
     }
     let button = document.querySelector("#number")
-
-
   }
 }
 
 // init
-function init() {
-render(document.getElementById("hangmans"), "")
-  document.getElementById("body").addEventListener("click", fun)
+function init(e) {
+  answer = ''
+  randomWord()
+  mistakes = 0
+  guessed = []
+  blank = ""
+  for (let i = 0; i < answer.length; i++) {
+    blank += "_ "
+  }
+  answerwithspaces = ""
+  for (let i = 0; i < answer.length; i++) {
+    answerwithspaces += answer[i]
+    answerwithspaces += " "
+  }
+  userRating
+  render(document.getElementById("blank"), blank)
+  clearBoard()
+  for (var i = 1; i < 27; i++) {
+    document.getElementById(i).disabled = false
+    document.getElementById(i).className = "button"
+  }
 
 }
-function game(){
-String.prototype.replaceAt = function (index, replacement) {
-  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-}
-// handleGuess
-function handleGuess() {
-  document.getElementById("body").addEventListener("click", fun)
 
+// clearBoard
+function clearBoard() {
+  render(document.getElementById("hangman1"), "")
+  render(document.getElementById("hangman2"), "")
+  render(document.getElementById("hangman3"), "")
+
+  render(document.getElementById("hangman4"), "")
+  render(document.getElementById("hangman5"), "")
+  render(document.getElementById("hangman6"), "")
+
+  render(document.getElementById("hangman7"), "")
+  render(document.getElementById("hangman8"), "")
+  render(document.getElementById("hangman9"), "")
+
+  render(document.getElementById("hangman10"), "")
+  render(document.getElementById("hangman11"), "")
+  render(document.getElementById("hangman12"), "")
+
+  render(document.getElementById("hangman13"), "")
+  render(document.getElementById("hangman14"), "")
+  render(document.getElementById("hangman15"), "")
+  render(document.getElementById("hangman16"), "")
 }
-handleGuess()
-}
-game()
-document.getElementById("reset").addEventListener("click", init);
+
+init()
+
+// sources
+// https://jsfiddle.net/3fzb1kk1/
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_change_innerhtml
+// https://css-tricks.com/forums/topic/making-my-h1-and-h2-text-vertically-closer-to-one-another/
+// https://stackoverflow.com/questions/14494747/how-to-add-images-to-readme-md-on-github
+// https://davidwalsh.name/event-delegate
+// https://www.twitchquotes.com/copypastas/ascii-art
